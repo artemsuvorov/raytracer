9230a9513022d5939c8f845590e2f2655db89a28
Artem Suvorov <suvorov.artem@hotmail.com>
2024-05-04 13:19:42 +0500

    Add GL hints during the initialization and enable VSync.

--- src/Core/Window.cpp
+++ src/Core/Window.cpp
@@ -38,14 +38,21 @@ void Window::Init(const WindowParams& params)
         const bool initialized = glfwInit();
         assert(initialized && "Failed to initialize GLFW!");
     }
-    
-    m_Handle = glfwCreateWindow(params.Width, params.Height, params.Title, nullptr, nullptr);
+
 #ifndef NDEBUG
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 #endif
+    
+    m_Handle = glfwCreateWindow(params.Width, params.Height, params.Title, nullptr, nullptr);
     assert(m_Handle && "Failed to initialize GLFW!");
     
     glfwMakeContextCurrent(m_Handle);
+
+	glfwSwapInterval(true);
+    glViewport(0, 0, params.Width, params.Height);
     
     const uint32_t status = glewInit();
     assert(status == GLEW_OK && "Failed to initialize GLEW!");
