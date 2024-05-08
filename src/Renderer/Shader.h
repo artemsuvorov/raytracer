2e60a289dc21e4bf71af01f8476bb874df82f9fb
Artem Suvorov <suvorov.artem@hotmail.com>
2024-05-08 13:04:08 +0500

    Refactor Shader.h

--- src/Renderer/Shader.h
+++ src/Renderer/Shader.h
@@ -2,51 +2,51 @@
 
 namespace Core {
 
-    // Forwards.
-    class ShaderBuilder;
+// Forwards.
+class ShaderBuilder;
 
-    // OpenGL Shader.
-    class Shader final
-    {
-    public:
-        static ShaderBuilder Create();
+// OpenGL Shader.
+class Shader final
+{
+public:
+    static ShaderBuilder Create();
 
-        Shader(uint32_t id);
-        ~Shader();
+    Shader(uint32_t id);
+    ~Shader();
 
-        void Bind() const;
-		void Unbind() const;
+    void Bind() const;
+    void Unbind() const;
 
-        void SetUniform(const std::string& name, int32_t value);
-		void SetUniform(const std::string& name, float value);
+    void SetUniform(const std::string& name, int32_t value);
+    void SetUniform(const std::string& name, float value);
 
-	private:
-		uint32_t m_Id;
-    };
+private:
+    uint32_t m_Id;
+};
 
-    enum class ShaderType
-    {
-        kNone,
-        kVertexShader,
-        kFragmentShader,
-        kComputeShader,
-    };
+enum class ShaderType
+{
+    kNone,
+    kVertexShader,
+    kFragmentShader,
+    kComputeShader,
+};
 
-    class ShaderBuilder final
-    {
-    public:
-        ShaderBuilder();
+class ShaderBuilder final
+{
+public:
+    ShaderBuilder();
 
-        ShaderBuilder& Attach(ShaderType type, const std::string& filepath);
-        std::shared_ptr<Shader> Link();
+    ShaderBuilder& Attach(ShaderType type, const std::string& filepath);
+    std::shared_ptr<Shader> Link();
 
-    private:
-        std::string ReadShader(const std::string& filepath);
-        uint32_t CompileShader(ShaderType type, const std::string& source);
+private:
+    std::string ReadShader(const std::string& filepath);
+    uint32_t CompileShader(ShaderType type, const std::string& source);
 
-    private:
-        uint32_t m_ProgramId;
-        std::vector<uint32_t> m_AttachedShaders;
-    };
+private:
+    uint32_t m_ProgramId;
+    std::vector<uint32_t> m_AttachedShaders;
+};
 
 }
